2b1d5cec087b8b823150ce6f9bf3f20d7fa10d17
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-06 21:44:22 +0530

    Function: Using Functions to Modify Web Pages

--- home.js
+++ home.js
@@ -1,19 +1,3 @@
-// Function: Function Scope
-// showMessage('Message')
-
-let key = 100;
-
-function message(value) {
-    let keyGenerator = function () {
-        let key = 10;
-        console.log('Key Generator: ', key);
-        return key;
-    }
-    let code = value * keyGenerator();
-    showMessage(code);
-    console.log('Code Generator: ', key);
-    return code
-}
-let getMessage = message(100);
-
-console.log(getMessage);
+// Function: Using Functions to Modify Web Pages
+showMessage('Message')
+showPercentage( 55 )
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 function showMessage(message) {
     document.getElementById('message').textContent = message;
 }
+
+function showPercentage(percentage){
+    document.getElementById('percentage').textContent = percentage + ' % OFF';
+}
