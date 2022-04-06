bbf48a11b04ac8f3bad2fb6ab0395b3d9c612894
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-06 16:07:34 +0530

    Variables and Constants: Naming Variables

--- home.js
+++ home.js
@@ -1,2 +1,17 @@
-let message = 'Welcome VedaGna'
-showMessage(message);
+// Naming Variables
+console.clear();
+/*
+a
+account
+account_99
+accountNumber
+_accountNumber
+$accountNumber
+_123
+_proto_
+*/
+let name = 'Welcome VedaGna';
+let price = 49.9;
+let discount = false;
+
+showMessage(price);
