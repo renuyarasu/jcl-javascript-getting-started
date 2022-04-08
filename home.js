c2e714ca5c39ff3f3da8088a4dac76260e43d1bd
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-08 12:12:57 +0530

    Objects and the DOM: Object Properties

--- home.js
+++ home.js
@@ -1,2 +1,12 @@
+let mySymbol = Symbol();
 
-showMessage('help');
+let person = {
+    name: 'VedaGna',
+    age: 5,
+    school: false,
+    [mySymbol]: 'secretInfo'
+}
+person['country'] = 'India';
+person.school=true;
+
+showMessage(person.country);
