2376e87f1f5db0162f0d72eacdb93010eabf4099
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-08 13:44:54 +0530

    Objects and the DOM: Showing and Hiding DOM Elements

--- home.js
+++ home.js
@@ -1,5 +1,12 @@
 const btnReview = document.getElementById('btnReview');
-btnReview.addEventListener('click', function(){
-    alert('message');
-})
 
+btnReview.addEventListener('click', function () {
+    const review = document.getElementById('review');
+    if(review.classList.contains('d-none')){
+        review.classList.remove('d-none');
+        btnReview.textContent='Close Review'
+    }else{
+        review.classList.add('d-none');
+        btnReview.textContent='See Review'
+    }
+})
