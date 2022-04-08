5e4f3ef98b3e2fceb5e286a7f01bc0347e566198
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-08 21:19:30 +0530

    Scope and Hoisting: var and Hoisting

--- home.js
+++ home.js
@@ -1,11 +1,10 @@
-function showProductID() {
-    let productID = 12345;
-    function fix() {
-        let productID = 67890;
-        console.log('Fix product Id: ', productID);
-    }
-    fix();
-    console.log('Show Product Id: ', productID);
-}
-showProductID();
+/* productID = 123;
 
+console.log(productID);
+
+let productID = 123; //Uncaught ReferenceError: Cannot access 'productID' before initialization
+ */
+productID()
+function productID(){
+    console.log(123);
+}
