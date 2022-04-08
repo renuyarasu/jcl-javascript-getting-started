e9ef3c85c3c87fbe3bb46888a5b2f935d1b9ec92
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-08 17:55:42 +0530

    Arrays: .forEach()

--- home.js
+++ home.js
@@ -1,6 +1,4 @@
-const values = ['a', 'b', 'hello', 'c', 'd', 'e'];
-
-const found = values.find(function (item) {
-    return item.length > 1
-});
-console.log(found); // hello
+const values = ['a', 'b', 'c', 'd', 'e'];
+values.forEach(function(item){
+    console.log(item);
+})
